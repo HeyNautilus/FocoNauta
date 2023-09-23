eb85114679e55cebc74ea6edb3ec753f87bdf690
Helena <129614934+HeyNautilus@users.noreply.github.com>
2023-09-23 09:00:27 -0300

    Delete FocoNauta_V1/simple-form_JAR directory

--- FocoNauta_V1/simple-form_JAR/src/main/java/com/simpleform/exceptions/CriptoExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.simpleform.exceptions;
-
-public class CriptoExistsException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public CriptoExistsException(String mg) {
-		super(mg);
-	}
-}
